a1c7dbe5a180ec4dce404410adeabab42ad134a8
Joonas Mielonen <Joonas.mielonen@metropolia.fi>
2021-04-15 09:10:08 +0300

    push to remote

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/images/hello-icon-192.png',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', (e) => {
+  e.waitUntil(async () => {
+    try {
+      const cache = await caches.open(cacheName);
+      return cache.addAll(filesToCache);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', (e) => {
+  e.respondWith(async () => {
+    try {
+      const response = await caches.match(e.request);
+      return response || fetch(e.request);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+});
