efb5d82268ab3ce92a960311d794d1eda430631e
Joonas Mielonen <Joonas.mielonen@metropolia.fi>
2021-04-15 09:27:35 +0300

    Update

--- public/sw.js
+++ public/sw.js
@@ -9,24 +9,28 @@ var filesToCache = [
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', (e) => {
-  e.waitUntil(async () => {
-    try {
-      const cache = await caches.open(cacheName);
-      return cache.addAll(filesToCache);
-    } catch (error) {
-      console.log(error.message);
-    }
-  });
+  e.waitUntil(
+    (async () => {
+      try {
+        const cache = await caches.open(cacheName);
+        return cache.addAll(filesToCache);
+      } catch (error) {
+        console.log(error.message);
+      }
+    })()
+  );
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', (e) => {
-  e.respondWith(async () => {
-    try {
-      const response = await caches.match(e.request);
-      return response || fetch(e.request);
-    } catch (error) {
-      console.log(error.message);
-    }
-  });
+  e.respondWith(
+    (async () => {
+      try {
+        const response = await caches.match(e.request);
+        return response || fetch(e.request);
+      } catch (error) {
+        console.log(error.message);
+      }
+    })()
+  );
 });
